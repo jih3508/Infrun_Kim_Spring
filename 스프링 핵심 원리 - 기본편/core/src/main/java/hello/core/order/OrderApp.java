753b71d1f59010268eb6f4276a0f674ac0112971
jih3508 <whdlrgus32@naver.com>
2021-11-05 12:40:41 +0900

    스프링 핵심 원리 이해1 - 예제 만들기

---
+++ 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/order/OrderApp.java
@@ -0,0 +1,23 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImple;
+
+public class OrderApp {
+
+    public static  void main(String[] args){
+        MemberService memberService = new MemberServiceImple();
+        OrderService orderService = new OrderServiceImpl();
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.CreateOrder(memberId, "itemA", 10000);
+
+        System.out.println("order = " + order);
+        System.out.println("order.calculatePrice = " + order.calculatePrice());
+    }
+}
